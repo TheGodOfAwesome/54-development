0899586b231c79e05545663b5ab61037b5dae656
Kuzivakwashe Muvezwa <muvezwakuzivakwashe@yahoo.com>
2024-10-02 06:21:09 +0200

    ui

--- src/app/AppWrappers.tsx
+++ src/app/AppWrappers.tsx
@@ -4,13 +4,15 @@ import 'styles/App.css';
 import 'styles/Contact.css';
 import 'styles/MiniCalendar.css';
 import { ChakraProvider } from '@chakra-ui/react';
-// import { CacheProvider } from '@chakra-ui/next-js';
+import { CacheProvider } from '@chakra-ui/next-js';
 import theme from '../theme/theme';
 
 export default function AppWrappers({ children }: { children: ReactNode }) {
   return (
     // <CacheProvider>
+    <>
       <ChakraProvider theme={theme}>{children}</ChakraProvider>{' '}
+    </>
     // </CacheProvider>
   );
 }
