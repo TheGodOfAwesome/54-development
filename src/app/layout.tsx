65980a809eced34ca520ad40926570a30c1def21
Kuzivakwashe Muvezwa <muvezwakuzivakwashe@yahoo.com>
2024-10-03 05:29:07 +0200

    radding landing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import { Box } from '@chakra-ui/react';
 import React, { ReactNode } from 'react';
 import AppWrappers from './AppWrappers';
+import { Providers } from "./providers";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body id={'root'}>
-        <AppWrappers>{children}</AppWrappers>
+        <AppWrappers>
+          <Providers>
+            {children}
+          </Providers>
+        </AppWrappers>
       </body>
     </html>
   );
